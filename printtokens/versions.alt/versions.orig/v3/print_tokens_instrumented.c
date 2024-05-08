dfef82ac54d949117dba57319cd0d3d7f356ebd6
m.khazaee <meysam.khazai@peykasa.ir>
2024-05-09 00:02:52 +0330

    rename output file name

--- printtokens/versions.alt/versions.orig/v3/print_tokens_instrumented.c
+++ printtokens/versions.alt/versions.orig/v3/print_tokens_instrumented.c
@@ -30,7 +30,7 @@ main(argc,argv)
 int argc;
 char *argv[];
 {   
-    file_name=fopen("logs.txt","a+"); 
+    file_name=fopen("v3.txt","a+"); 
     if(!file_name)
     {	
         printf("File could not be opened! \n");
