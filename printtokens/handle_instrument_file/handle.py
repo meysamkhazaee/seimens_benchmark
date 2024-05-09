a4471766122d5c1cabbf2919756cd1529cc731df
m.khazaee <meysam.khazai@peykasa.ir>
2024-05-09 10:02:16 +0330

    fix some bugs and complete execution

--- printtokens/handle_instrument_file/handle.py
+++ printtokens/handle_instrument_file/handle.py
@@ -1,15 +1,19 @@
 import sys
+import re
 
 def process_file(filename):
     with open(filename, 'r') as infile:
         lines = infile.readlines()
+    lines.pop(0) # delete first line because first line of all paths instrument files are empty line at the start of file
+    lines[-1] = lines[-1] + "\n" # add enter to last line
     seen = set()
     result = []
     for line_number, line in enumerate(lines):
-        if line not in seen:
-            seen.add(line)
-            result.append(f"T{line_number+1}: {line[:-1]}\n")
-    with open(filename + '.numbered.txt', 'w') as outfile:
+      if line not in seen:
+        seen.add(line)
+        result.append(f"T{line_number}: {line[:-2]}\n")
+    result[-1] = result[-1][:-1]
+    with open(filename[:-4] + '.optimal.txt', 'w') as outfile:
         outfile.writelines(result)
 
 def main():
--- printtokens/scripts/handler.sh
+++ printtokens/scripts/handler.sh
@@ -10,11 +10,13 @@ cp runner.sh runner_instrumented.sh ../source.alt/source.orig
 cd ../source.alt/source.orig
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code source.orig"
+echo -e ""
+echo -e ">>>>>>> run raw_code source.orig"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code source.orig"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code source.orig"
 ./runner_instrumented.sh $ROOT     
 rm runner.sh runner_instrumented.sh
 
@@ -26,11 +28,13 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v1
 cd ../versions.alt/versions.orig/v1
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v1"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v1"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v1"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v1"
 ./runner_instrumented.sh $ROOT  
 rm runner.sh runner_instrumented.sh
 
@@ -42,11 +46,13 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v2
 cd ../versions.alt/versions.orig/v2
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v2"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v2"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v2"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v2"
 ./runner_instrumented.sh $ROOT    
 rm runner.sh runner_instrumented.sh
 
@@ -57,11 +63,13 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v3
 cd ../versions.alt/versions.orig/v3
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v3"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v3"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v3"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v3"
 ./runner_instrumented.sh $ROOT   
 rm runner.sh runner_instrumented.sh
 
@@ -72,11 +80,13 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v4
 cd ../versions.alt/versions.orig/v4
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v4"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v4"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v4"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v4"
 ./runner_instrumented.sh $ROOT     
 rm runner.sh runner_instrumented.sh
 
@@ -87,11 +97,13 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v5
 cd ../versions.alt/versions.orig/v5
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v4"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v4"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v4"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v4"
 ./runner_instrumented.sh $ROOT   
 rm runner.sh runner_instrumented.sh
 
@@ -101,13 +113,16 @@ cd ../../..
 cd scripts
 cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v6
 cd ../versions.alt/versions.orig/v6
-echo ">>>>>>> run raw_code versions.orig/v6"
+echo -e ">>>>>>> run raw_code versions.orig/v6"
 echo -e ""
 echo -e "----------------------"
+echo -e ""
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v6"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v6"
+
 ./runner_instrumented.sh $ROOT   
 rm runner.sh runner_instrumented.sh
 
@@ -119,10 +134,37 @@ cp runner.sh runner_instrumented.sh ../versions.alt/versions.orig/v7
 cd ../versions.alt/versions.orig/v7
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run raw_code versions.orig/v7"
+echo -e ""
+echo -e ">>>>>>> run raw_code versions.orig/v7"
 ./runner.sh $ROOT
 echo -e ""
 echo -e "----------------------"
-echo ">>>>>>> run instrumented_code versions.orig/v7"
+echo -e ""
+echo -e ">>>>>>> run instrumented_code versions.orig/v7"
 ./runner_instrumented.sh $ROOT    
 rm runner.sh runner_instrumented.sh
+
+cd ../../..
+echo -e ""
+echo -e "----------------------"
+echo -e ""
+echo -e ">>>>>>> optimize instrumation result of all executions"
+cp source.alt/source.orig/result/v0.txt \
+   versions.alt/versions.orig/v1/result/v1.txt \
+   versions.alt/versions.orig/v2/result/v2.txt \
+   versions.alt/versions.orig/v3/result/v3.txt \
+   versions.alt/versions.orig/v4/result/v4.txt \
+   versions.alt/versions.orig/v5/result/v5.txt \
+   versions.alt/versions.orig/v6/result/v6.txt \
+   versions.alt/versions.orig/v7/result/v7.txt handle_instrument_file/
+
+cd handle_instrument_file
+python3 handle.py v0.txt
+python3 handle.py v1.txt
+python3 handle.py v2.txt
+python3 handle.py v3.txt
+python3 handle.py v4.txt
+python3 handle.py v5.txt
+python3 handle.py v6.txt
+python3 handle.py v7.txt
+
