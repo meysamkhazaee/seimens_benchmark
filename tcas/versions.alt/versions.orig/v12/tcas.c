6ff62669dc28da43403057fb4784cf80f360eb16
m.khazaee <meysam.khazai@peykasa.ir>
2024-06-26 19:17:27 +0330

    compare first edition of tcas code

--- tcas/versions.alt/versions.orig/v12/tcas.c
+++ tcas/versions.alt/versions.orig/v12/tcas.c
@@ -1,10 +1,3 @@
-
-/*  -*- Last-Edit:  Tue Apr 20 11:22:37 1993 by Michael Greenberg; -*- */
-/* $Log: tcas.c,v $
- * Revision 1.2  1993/03/12  19:29:50  foster
- * Correct logic bug which didn't allow output of 2 - hf
- * */
-
 #include <stdio.h>
 
 #define OLEV       600		/* in feets/minute */
@@ -29,7 +22,6 @@ int Positive_RA_Alt_Thresh[4];
 int Up_Separation;
 int Down_Separation;
 
-				/* state variables */
 int Other_RAC;			/* NO_INTENT, DO_NOT_CLIMB, DO_NOT_DESCEND */
 #define NO_INTENT 0
 #define DO_NOT_CLIMB 1
@@ -55,7 +47,7 @@ void initialize()
 
 int ALIM ()
 {
- return Positive_RA_Alt_Thresh[Alt_Layer_Value];
+    return Positive_RA_Alt_Thresh[Alt_Layer_Value];
 }
 
 int Inhibit_Biased_Climb ()
@@ -72,11 +64,11 @@ bool Non_Crossing_Biased_Climb()
     upward_preferred = Inhibit_Biased_Climb() > Down_Separation;
     if (upward_preferred)
     {
-	result = !(Own_Below_Threat()) || ((Own_Below_Threat()) && (!(Down_Separation >= ALIM())));
+	    result = !(Own_Below_Threat()) || ((Own_Below_Threat()) && (!(Down_Separation >= ALIM())));
     }
     else
     {	
-	result = Own_Above_Threat() && (Cur_Vertical_Sep >= MINSEP) && (Up_Separation >= ALIM());
+	    result = Own_Above_Threat() && (Cur_Vertical_Sep >= MINSEP) && (Up_Separation >= ALIM());
     }
     return result;
 }
@@ -90,11 +82,11 @@ bool Non_Crossing_Biased_Descend()
     upward_preferred = Inhibit_Biased_Climb() > Down_Separation;
     if (upward_preferred)
     {
-	result = Own_Below_Threat() && (Cur_Vertical_Sep >= MINSEP) && (Down_Separation >= ALIM());
+	    result = Own_Below_Threat() && (Cur_Vertical_Sep >= MINSEP) && (Down_Separation >= ALIM());
     }
     else
     {
-	result = !(Own_Above_Threat()) || ((Own_Above_Threat()) && (Up_Separation >= ALIM()));
+	    result = !(Own_Above_Threat()) || ((Own_Above_Threat()) && (Up_Separation >= ALIM()));
     }
     return result;
 }
@@ -123,19 +115,27 @@ int alt_sep_test()
     
     if (enabled && ((tcas_equipped && intent_not_known) || !tcas_equipped))
     {
-	need_upward_RA = Non_Crossing_Biased_Climb() && Own_Below_Threat();
-	need_downward_RA = Non_Crossing_Biased_Descend() && Own_Above_Threat();
-	if (need_upward_RA && need_downward_RA)
-        /* unreachable: requires Own_Below_Threat and Own_Above_Threat
-           to both be true - that requires Own_Tracked_Alt < Other_Tracked_Alt
-           and Other_Tracked_Alt < Own_Tracked_Alt, which isn't possible */
-	    alt_sep = UNRESOLVED;
-	else if (need_upward_RA)
-	    alt_sep = UPWARD_RA;
-	else if (need_downward_RA)
-	    alt_sep = DOWNWARD_RA;
-	else
-	    alt_sep = UNRESOLVED;
+        need_upward_RA = Non_Crossing_Biased_Climb() && Own_Below_Threat();
+        need_downward_RA = Non_Crossing_Biased_Descend() && Own_Above_Threat();
+        if (need_upward_RA && need_downward_RA)
+        {
+            /* unreachable: requires Own_Below_Threat and Own_Above_Threat
+            to both be true - that requires Own_Tracked_Alt < Other_Tracked_Alt
+            and Other_Tracked_Alt < Own_Tracked_Alt, which isn't possible */
+            alt_sep = UNRESOLVED;
+        }
+        else if (need_upward_RA)
+        {
+            alt_sep = UPWARD_RA;
+        }
+        else if (need_downward_RA)
+        {
+            alt_sep = DOWNWARD_RA;
+        }
+        else
+        {
+            alt_sep = UNRESOLVED;
+        }    
     }
     
     return alt_sep;
@@ -147,12 +147,12 @@ char *argv[];
 {
     if(argc < 13)
     {
-	fprintf(stdout, "Error: Command line arguments are\n");
-	fprintf(stdout, "Cur_Vertical_Sep, High_Confidence, Two_of_Three_Reports_Valid\n");
-	fprintf(stdout, "Own_Tracked_Alt, Own_Tracked_Alt_Rate, Other_Tracked_Alt\n");
-	fprintf(stdout, "Alt_Layer_Value, Up_Separation, Down_Separation\n");
-	fprintf(stdout, "Other_RAC, Other_Capability, Climb_Inhibit\n");
-	exit(1);
+        fprintf(stdout, "Error: Command line arguments are\n");
+        fprintf(stdout, "Cur_Vertical_Sep, High_Confidence, Two_of_Three_Reports_Valid\n");
+        fprintf(stdout, "Own_Tracked_Alt, Own_Tracked_Alt_Rate, Other_Tracked_Alt\n");
+        fprintf(stdout, "Alt_Layer_Value, Up_Separation, Down_Separation\n");
+        fprintf(stdout, "Other_RAC, Other_Capability, Climb_Inhibit\n");
+        exit(1);
     }
     initialize();
     Cur_Vertical_Sep = atoi(argv[1]);
